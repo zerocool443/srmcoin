19055fa79d5f4d9f1fc5e20a5f631d9af16d0939
Chirag Jariwala <cjhackerz@outlook.com>
2017-12-12 17:40:01 +0530

    Added value to GENESIS_COINBASE_TX_HEX[]

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -74,7 +74,7 @@ const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json"
 
 //TODO Put here the name of your currency
 const char     CRYPTONOTE_NAME[]                             = "srmcoin";
-const char     GENESIS_COINBASE_TX_HEX[]                     = "";
+const char     GENESIS_COINBASE_TX_HEX[]                     = "013c01ff0001f011029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd0880712101cacecad849d1615aa3ae7a6ecdd862adc94823f3104ea56ae48e858dfeeb8905";
 
 const uint8_t  CURRENT_TRANSACTION_VERSION                   =  1;
 const uint8_t  BLOCK_MAJOR_VERSION_1                         =  1;
